246fa48f260e4d5fd60845f3aec80c108e4b3b99
moonspiritacm <yefanbupt@gmail.com>
2023-09-17 19:02:30 +0800

    Standard Type Demo

---
+++ MyTextBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Calculator_NET
+{
+    internal sealed class MyTextBox : TextBox
+    {
+        protected override void OnPreviewGotKeyboardFocus(KeyboardFocusChangedEventArgs e)
+        {
+            e.Handled = true;
+            base.OnPreviewGotKeyboardFocus(e);
+        }
+    }
+}
